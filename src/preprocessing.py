ef087ed77eecd2687bb5e793d6b45251b47b384b
lheyer <49233686+lheyer@users.noreply.github.com>
2022-02-07 14:43:53 +0100

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -116,7 +116,7 @@ class Meteo_DS(Dataset):
     if 'depth' in self.Y.columns:
       index_arr = np.array(['temp_'+str(i) for i in np.arange(0,self.n_depths*0.5,0.5)])
       Y_labels = self.Y[['date','depth','temp']]
-      Y_labels = Y_labels.depth.apply(lambda x: round(x * 2) / 2)
+      Y_labels.depth = Y_labels.depth.apply(lambda x: round(x * 2) / 2)
       Y_labels = Y_labels.pivot_table(index='date',columns=['depth'],values=['temp'])
       Y_labels.columns = ["_".join((i,str(j))) for i,j in Y_labels.columns]
       Y_labels = Y_labels.T
