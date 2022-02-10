c4249c3ae6527e6d97186b163bbb4091912c5749
lheyer <github@lheyer.de>
2022-02-10 11:23:29 +0100

    a lot of automatic code formatting, forgot that pretrain_labels are formatted like predictions, got rid of some warnings due to use of .pd.DataFrame.append and made plot names more readable

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1,4 +1,4 @@
-# 
+#
 import pandas as pd
 import numpy as np
 import preprocessing as pp
@@ -9,8 +9,10 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 
-parser.add_argument('-d',  type=str,help='dataset type ("similar","year","season")')
-parser.add_argument('-train_type',  type=str,help='dataset type ("train","no_pretrain","pretrain"',default='train')
+parser.add_argument(
+    '-d',  type=str, help='dataset type ("similar","year","season")')
+parser.add_argument('-train_type',  type=str,
+                    help='dataset type ("train","no_pretrain","pretrain"', default='train')
 
 
 args = parser.parse_args()
@@ -20,76 +22,84 @@ train_type = args.train_type
 depth_areas = pp.lake_depth_areas_dict['Lake Mendota']
 
 dataset_type = args.d
-if train_type=='train':
-  predictions_similar = list([])
-  rsme_similar = list([])
-  dates_similar = list([])
-  for exper_n in range(2):
-    exper_n +=1
-    print(exper_n)
+if train_type == 'train':
+    predictions_similar = list([])
+    rsme_similar = list([])
+    dates_similar = list([])
+    for exper_n in range(2):
+        exper_n += 1
+        print(exper_n)
 
+        meteo_path = 'data/pretrain/mendota_meteo.csv'
+        test_data_path = 'data/test_splitted/me_test_' + \
+            dataset_type+'_exper_'+str(exper_n)+'.csv'
+        depth_areas = pp.lake_depth_areas_dict['Lake Mendota']
+        ice_flags_path = 'data/pretrain/mendota_pretrainer_ice_flags.csv'
+        model_path = 'model/model_train_' + \
+            dataset_type+'_exper_'+str(exper_n)+'.model'
+
+        print(test_data_path)
+
+        predictions, rsme, dates = eval(
+            model_path, meteo_path, test_data_path, depth_areas, ice_flags_path)
+        predictions_similar.append([predictions])
+        rsme_similar.append([rsme])
+        dates_similar.append([dates])
+
+    test_df = pd.DataFrame()
+    for exper_n in range(2):
+        exper_n += 1
+        test_data_path = 'data/test_splitted/me_test_' + \
+            dataset_type+'_exper_'+str(exper_n)+'.csv'
+
+        new_df = pd.read_csv(test_data_path, parse_dates=['date'])
+        # test_df = test_df.append(new_df)
+        test_df = pd.concat([test_df, new_df], axis=0)
+
+    vis.plot_ts(predictions_similar, dates_similar, test_df=test_df, title='Predictions of model trained with ' +
+                dataset_type+' dataset on test dataset vs labels', labels=None, savepath='pictures', plotname='ts_'+dataset_type)
+
+elif train_type == 'no_pretrain':
     meteo_path = 'data/pretrain/mendota_meteo.csv'
-    test_data_path = 'data/test_splitted/me_test_'+dataset_type+'_exper_'+str(exper_n)+'.csv'
+    test_data_path = 'data/test_splitted/me_test_'+dataset_type+'_exper_1.csv'
     depth_areas = pp.lake_depth_areas_dict['Lake Mendota']
     ice_flags_path = 'data/pretrain/mendota_pretrainer_ice_flags.csv'
-    model_path = 'model/model_train_'+dataset_type+'_exper_'+str(exper_n)+'.model'
+    model_path = 'model/model_train_'+dataset_type+'_exper_1_no_pt.model'
 
     print(test_data_path)
 
-    predictions, rsme, dates = eval(model_path, meteo_path, test_data_path, depth_areas, ice_flags_path)
-    predictions_similar.append([predictions])
-    rsme_similar.append([rsme])
-    dates_similar.append([dates])
+    predictions, rsme, dates = eval(
+        model_path, meteo_path, test_data_path, depth_areas, ice_flags_path)
 
-  test_df = pd.DataFrame()
-  for exper_n in range(2):
-    exper_n +=1
-    test_data_path = 'data/test_splitted/me_test_'+dataset_type+'_exper_'+str(exper_n)+'.csv'
+    print('RSME: ', rsme)
 
-    new_df = pd.read_csv(test_data_path,parse_dates=['date'])
-    test_df = test_df.append(new_df)
+    test_data_path = 'data/test_splitted/me_test_'+dataset_type+'_exper_1.csv'
 
-  vis.plot_ts(predictions_similar, dates_similar, test_df=test_df,title='Predictions of model trained with '+dataset_type+' dataset on test dataset vs labels',labels=None,savepath='pictures',plotname='ts_'+dataset_type) 
+    test_df = pd.read_csv(test_data_path, parse_dates=['date'])
 
-elif train_type=='no_pretrain':
-  meteo_path = 'data/pretrain/mendota_meteo.csv'
-  test_data_path = 'data/test_splitted/me_test_'+dataset_type+'_exper_1.csv'
-  depth_areas = pp.lake_depth_areas_dict['Lake Mendota']
-  ice_flags_path = 'data/pretrain/mendota_pretrainer_ice_flags.csv'
-  model_path = 'model/model_train_'+dataset_type+'_exper_1_no_pt.model'
+    vis.plot_ts(predictions_similar, dates_similar, test_df=test_df, title='Predictions of model (no pretraining) trained with ' +
+                dataset_type+' dataset on test dataset vs labels', labels=None, savepath='pictures', plotname='ts_'+dataset_type+'_no_pt_')
 
-  print(test_data_path)
+elif train_type == 'pretrain':
+    meteo_path = 'data/pretrain/mendota_meteo.csv'
+    test_data_path = 'data/predictions/me_predict_pb0.csv'
+    depth_areas = pp.lake_depth_areas_dict['Lake Mendota']
+    ice_flags_path = 'data/pretrain/mendota_pretrainer_ice_flags.csv'
+    model_path = 'model/model_pretrain_pgdl_ec01_400_til2009.model'
 
-  predictions, rsme, dates = eval(model_path, meteo_path, test_data_path, depth_areas, ice_flags_path)
-  
-  print('RSME: ',rsme)
-  
-  test_data_path = 'data/test_splitted/me_test_'+dataset_type+'_exper_1.csv'
+    print(test_data_path)
 
-  test_df = pd.read_csv(test_data_path,parse_dates=['date'])
-  
-  vis.plot_ts(predictions_similar, dates_similar, test_df=test_df,title='Predictions of model (no pretraining) trained with '+dataset_type+' dataset on test dataset vs labels',labels=None,savepath='pictures',plotname='ts_'+dataset_type+'_no_pt_')
-  
-elif train_type=='pretrain':
-  meteo_path = 'data/pretrain/mendota_meteo.csv'
-  test_data_path = 'data/predictions/me_predict_pb0.csv'
-  depth_areas = pp.lake_depth_areas_dict['Lake Mendota']
-  ice_flags_path = 'data/pretrain/mendota_pretrainer_ice_flags.csv'
-  model_path ='model/model_pretrain_pgdl_ec01_400_til2009.model'
-  
-  print(test_data_path)
+    predictions, rsme, dates = eval(
+        model_path, meteo_path, test_data_path, depth_areas, ice_flags_path)
 
-  predictions, rsme, dates = eval(model_path, meteo_path, test_data_path, depth_areas, ice_flags_path)
-  
-  print('RSME: ',rsme)
-  
-  test_data_path = 'data/test_splitted/me_test_'+dataset_type+'_exper_1.csv'
+    print('RSME: ', rsme)
 
-  new_df = pd.read_csv(test_data_path,parse_dates=['date'])
-  
-  vis.plot_depth_ts(prediction_df,dates,title='',test_data=False,savepath='pictures',plotname='dp_ts_pretrain')
-  
-  vis.plot_depth_ts(new_df,dates,title='',test_data=False,savepath='pictures',plotname='dp_ts_pretrain')
-  
-  
-  
+    #test_data_path = 'data/test_splitted/me_test_'+dataset_type+'_exper_1.csv'
+
+    new_df = pd.read_csv(test_data_path, parse_dates=['date'])
+
+    vis.plot_depth_ts(predictions, dates, title='', test_data=False,
+                      savepath='pictures', plotname='pretrain')
+
+    vis.plot_depth_ts(new_df, new_df.date.values, title='', test_data=False,
+                      savepath='pictures', plotname='pretrain_labels')
--- src/visualize.py
+++ src/visualize.py
@@ -24,102 +24,110 @@ from os.path import join
 #   return new_df
 
 
-def stack_depth_cols_preds(df,dates):
-  new_df = pd.DataFrame(columns=['date','depth','temp'])
-  for col in df.columns:
-    d = col.split('_')[1]
-    #print(d)
-    #print(train_dataset.XY[train_dataset.XY.depth==d].index.shape)
+def stack_depth_cols_preds(df, dates):
+    new_df = pd.DataFrame(columns=['date', 'depth', 'temp'])
+    for col in df.columns:
+        if col != 'date':
 
-    tmp_df = pd.DataFrame(columns=['date','depth','temp'])
-    tmp_df.temp = df[col]
-    #print(tmp_df.shape)
-    tmp_df.date = dates
-    tmp_df.depth = float(col.split('_')[1])
-    new_df = new_df.append(tmp_df)
+            d = col.split('_')[1]
+            # print(d)
+            # print(train_dataset.XY[train_dataset.XY.depth==d].index.shape)
 
-  return new_df
+            tmp_df = pd.DataFrame(columns=['date', 'depth', 'temp'])
+            tmp_df.temp = df[col]
+            # print(tmp_df.shape)
+            tmp_df.date = dates
+            tmp_df.depth = float(col.split('_')[1])
+            # new_df = new_df.append(tmp_df)
+            new_df = pd.concat([new_df, tmp_df], axis=0)
+        else:
+            pass
 
-def plot_depth_ts(prediction_df,dates,title='',test_data=False,savepath=None,plotname=''):
-
-  # new_df = pd.DataFrame(columns=['date','depth','temp'])
-  # for col in prediction_df.columns:
-  #   d = col.split('_')[1]
-  #   #print(d)
-  #   #print(train_dataset.XY[train_dataset.XY.depth==d].index.shape)
-
-  #   tmp_df = pd.DataFrame(columns=['date','depth','temp'])
-  #   tmp_df.temp = prediction_df[col]
-  #   #print(tmp_df.shape)
-  #   tmp_df.date = dates
-  #   tmp_df.depth = float(col.split('_')[1])
-  #   new_df = new_df.append(tmp_df)
-
-  if test_data:
-    new_df = prediction_df
-  else:
-    new_df = stack_depth_cols_preds(prediction_df,dates)
-
-  plt.figure(figsize=(30,5))
-
-  plt.title(title,fontsize=20)
-
-  ax = sns.scatterplot(data=new_df.set_index('date'), x="date", y="depth",hue="temp",palette='viridis',s=60,marker='s',linewidth=0)
-  ax.yaxis.label.set_size(20)
-  ax.tick_params(axis='y', labelsize=16)
-  ax.xaxis.label.set_size(20)
-  ax.tick_params(axis='x', labelsize=16)
-  plt.legend(loc='best', prop={'size': 16},bbox_to_anchor=(0.55, 0.5, 0.5, 0.5))
-  
-  if savepath is not None:
-    plt_name = plotname + 'plot_depth_ts.png'
-    savepath = join(savepath,plt_name)
-
-    plt.savefig(savepath)
-    
-  return 
-    
-    
+    return new_df
 
 
+def plot_depth_ts(prediction_df, dates, title='', test_data=False, savepath=None, plotname=''):
 
-def plot_ts(prediction_df_list, pred_date_list, test_df=None,title=None,labels=None,savepath=None,plotname=''):
+    # new_df = pd.DataFrame(columns=['date','depth','temp'])
+    # for col in prediction_df.columns:
+    #   d = col.split('_')[1]
+    #   #print(d)
+    #   #print(train_dataset.XY[train_dataset.XY.depth==d].index.shape)
 
-  plt.figure(figsize=(30,5))
-  if title is None:
-    plt.title('Predictions of pretrained model on training data and labels, all averaged over depth',fontsize=20)
-  else:
-    plt.title(title,fontsize=20)
+    #   tmp_df = pd.DataFrame(columns=['date','depth','temp'])
+    #   tmp_df.temp = prediction_df[col]
+    #   #print(tmp_df.shape)
+    #   tmp_df.date = dates
+    #   tmp_df.depth = float(col.split('_')[1])
+    #   new_df = new_df.append(tmp_df)
 
-  if labels is None:
-    #print(len(prediction_df_list))
-    labels = ['sampel '+str(i) for i in range(len(prediction_df_list))]
-    print(labels)
+    if test_data:
+        new_df = prediction_df
+    else:
+        new_df = stack_depth_cols_preds(prediction_df, dates)
 
-  ind = 0
-  for df,dates in zip(prediction_df_list,pred_date_list):
-    
-    new_df = stack_depth_cols_preds(df[0],dates[0])
+    plt.figure(figsize=(30, 5))
 
-    ax = new_df.groupby('date').mean().temp.plot(label=labels[ind],marker='.',ls='')
-    ind+=1
+    plt.title(title, fontsize=20)
 
-  if test_df is not None:
-    #new_df = stack_depth_cols_test_labels(test_df)
-    test_df.groupby('date').mean().temp.plot(label='labels',marker='.',ls='')
+    ax = sns.scatterplot(data=new_df.set_index('date'), x="date", y="depth",
+                         hue="temp", palette='viridis', s=60, marker='s', linewidth=0)
+    ax.yaxis.label.set_size(20)
+    ax.tick_params(axis='y', labelsize=16)
+    ax.xaxis.label.set_size(20)
+    ax.tick_params(axis='x', labelsize=16)
+    plt.legend(loc='best', prop={'size': 16},
+               bbox_to_anchor=(0.55, 0.5, 0.5, 0.5))
 
-  ax.yaxis.label.set_size(20)
-  ax.tick_params(axis='y', labelsize=16)
-  ax.xaxis.label.set_size(20)
-  ax.tick_params(axis='x', labelsize=16)
-  plt.legend(loc='best', prop={'size': 16})
-  plt.ylabel('T [°C]')
-  plt.xlabel('date')
-  
-  if savepath is not None:
-    plt_name = plotname + 'plot_ts.png'
-    savepath = join(savepath,plt_name)
-   
-    plt.savefig(savepath)
-    
-  return
+    if savepath is not None:
+        plt_name = plotname + '_plot_depth_ts.png'
+        savepath = join(savepath, plt_name)
+
+        plt.savefig(savepath)
+
+    return
+
+
+def plot_ts(prediction_df_list, pred_date_list, test_df=None, title=None, labels=None, savepath=None, plotname=''):
+
+    plt.figure(figsize=(30, 5))
+    if title is None:
+        plt.title(
+            'Predictions of pretrained model on training data and labels, all averaged over depth', fontsize=20)
+    else:
+        plt.title(title, fontsize=20)
+
+    if labels is None:
+        # print(len(prediction_df_list))
+        labels = ['sampel '+str(i) for i in range(len(prediction_df_list))]
+        print(labels)
+
+    ind = 0
+    for df, dates in zip(prediction_df_list, pred_date_list):
+
+        new_df = stack_depth_cols_preds(df[0], dates[0])
+
+        ax = new_df.groupby('date').mean().temp.plot(
+            label=labels[ind], marker='.', ls='')
+        ind += 1
+
+    if test_df is not None:
+        #new_df = stack_depth_cols_test_labels(test_df)
+        test_df.groupby('date').mean().temp.plot(
+            label='labels', marker='.', ls='')
+
+    ax.yaxis.label.set_size(20)
+    ax.tick_params(axis='y', labelsize=16)
+    ax.xaxis.label.set_size(20)
+    ax.tick_params(axis='x', labelsize=16)
+    plt.legend(loc='best', prop={'size': 16})
+    plt.ylabel('T [°C]')
+    plt.xlabel('date')
+
+    if savepath is not None:
+        plt_name = plotname + '_plot_ts.png'
+        savepath = join(savepath, plt_name)
+
+        plt.savefig(savepath)
+
+    return
