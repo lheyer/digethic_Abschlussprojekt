b436981eccb0b110d03f326256fb63529d79e1ce
lheyer <github@lheyer.de>
2022-02-20 18:34:07 +0100

    added time slice feture

--- src/eval.py
+++ src/eval.py
@@ -7,45 +7,54 @@ import preprocessing as pp
 from preprocessing import Meteo_DS as gMeteo_DS
 from model import GeneralLSTM
 
-def eval(model_path, meteo_path, test_data_path, depth_areas, ice_flags_path, state_size=20, begin_loss_ind=50):
-  
-  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-  torch.set_default_dtype(torch.float32)
 
-  
-  test_dataset = gMeteo_DS(meteo_path,test_data_path,depth_areas,ice_csv_path=ice_flags_path,transform=True,testing=True)
+def eval(model_path, meteo_path, test_data_path, depth_areas, ice_flags_path, time_slice=None, state_size=20, begin_loss_ind=50):
 
-  dates = test_dataset.XY.date
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    torch.set_default_dtype(torch.float32)
 
-  # initialize model
-  input_size = torch.Tensor(test_dataset.Xt).size()[-1]
-  batch_size = torch.Tensor(test_dataset.Xt).size()[-1]
-  model = GeneralLSTM(input_size, state_size,batch_size,device, num_layers=1)
-  criterion = torch.nn.MSELoss()#
-  #optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
-  model.to(device)
+    if time_slice is not None:
+        test_dataset = gMeteo_DS(meteo_path, test_data_path, depth_areas,
+                                 ice_csv_path=ice_flags_path, time_slice=time_slice, transform=True, testing=True)
+    else:
+        test_dataset = gMeteo_DS(meteo_path, test_data_path, depth_areas,
+                                 ice_csv_path=ice_flags_path, transform=True, testing=True)
 
-  #load trained model
-  pretrain_dict = torch.load(model_path)['state_dict']
-  model_dict = model.state_dict()
-  pretrain_dict = {k: v for k, v in pretrain_dict.items() if k in model_dict}
-  model_dict.update(pretrain_dict)
-  model.load_state_dict(pretrain_dict)
+    dates = test_dataset.XY.date
 
-  model.eval()
+    # initialize model
+    input_size = torch.Tensor(test_dataset.Xt).size()[-1]
+    batch_size = torch.Tensor(test_dataset.Xt).size()[-1]
+    model = GeneralLSTM(input_size, state_size,
+                        batch_size, device, num_layers=1)
+    criterion = torch.nn.MSELoss()
+    #optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
+    model.to(device)
 
-  with torch.no_grad(): #disable gradient
-    model.hidden = model.init_hidden(batch_size=torch.Tensor(test_dataset.Xt).size()[0])
-    h_state = None
-    preds,h_state = model(torch.Tensor(test_dataset.Xt))
-    #print(preds.size())
-    #print(torch.Tensor(train_dataset.labels).size())
-    loss_preds = preds[:, begin_loss_ind:,0]
-    loss_Y = torch.Tensor(test_dataset.labels)[:, begin_loss_ind:]
-    d_loss = criterion(loss_preds[~torch.isnan(loss_Y)], loss_Y[~torch.isnan(loss_Y)])
-    print("rmse=", np.sqrt(d_loss))
+    # load trained model
+    pretrain_dict = torch.load(model_path)['state_dict']
+    model_dict = model.state_dict()
+    pretrain_dict = {k: v for k, v in pretrain_dict.items() if k in model_dict}
+    model_dict.update(pretrain_dict)
+    model.load_state_dict(pretrain_dict)
 
-    depth_labels = ['temp_'+str(i) for i in test_dataset.XY.iloc[0].depths]
-    preds_df = pd.DataFrame(preds.detach().numpy()[:,:,0].T,columns=depth_labels)
+    model.eval()
 
-  return preds_df, np.sqrt(d_loss), dates
+    with torch.no_grad():  # disable gradient
+        model.hidden = model.init_hidden(
+            batch_size=torch.Tensor(test_dataset.Xt).size()[0])
+        h_state = None
+        preds, h_state = model(torch.Tensor(test_dataset.Xt))
+        # print(preds.size())
+        # print(torch.Tensor(train_dataset.labels).size())
+        loss_preds = preds[:, begin_loss_ind:, 0]
+        loss_Y = torch.Tensor(test_dataset.labels)[:, begin_loss_ind:]
+        d_loss = criterion(loss_preds[~torch.isnan(
+            loss_Y)], loss_Y[~torch.isnan(loss_Y)])
+        print("rmse=", np.sqrt(d_loss))
+
+        depth_labels = ['temp_'+str(i) for i in test_dataset.XY.iloc[0].depths]
+        preds_df = pd.DataFrame(preds.detach().numpy()[
+                                :, :, 0].T, columns=depth_labels)
+
+    return preds_df, np.sqrt(d_loss), dates
